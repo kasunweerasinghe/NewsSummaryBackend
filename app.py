708d3cb1962a65c8c9cfe01f610ce1d5d52ffb57
Kasun-Weerasinghe <kasunrachintha2018@gmail.com>
2024-01-28 15:04:19 +0530

    update code

--- app.py
+++ app.py
@@ -1,67 +1,3 @@
-# from fastapi import FastAPI, HTTPException
-# from fastapi.middleware.cors import CORSMiddleware
-# from pydantic import BaseModel
-# from textblob import TextBlob
-# from newspaper import Article
-#
-#
-#
-# app = FastAPI()
-#
-# origins = [
-#     "http://localhost:4200",
-# ]
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-#
-# class SummaryRequest(BaseModel):
-#     url: str
-#
-# class SummaryResponse(BaseModel):
-#     summary: str
-#     title: str
-#     author: str
-#     publication_date: str
-#     sentiment: dict
-#
-#
-# @app.post("/summarize", response_model=SummaryResponse)
-# def summarize(data: SummaryRequest):
-#     url = data.url
-#
-#     # Extract information using Newspaper3k
-#     article = Article(url)
-#     article.download()
-#     article.parse()
-#     article.nlp()
-#
-#     summary = article.summary
-#     title = article.title
-#     author = article.authors
-#     publication_date = article.publish_date
-#
-#     # Perform sentiment analysis using TextBlob
-#     analysis = TextBlob(article.text)
-#     sentiment = {
-#         'polarity': analysis.polarity,
-#         'sentiment': 'positive' if analysis.polarity > 0 else 'negative' if analysis.polarity < 0 else 'neutral'
-#     }
-#
-#     return {
-#         'summary': summary,
-#         'title': title,
-#         'author': author,
-#         'publication_date': publication_date,
-#         'sentiment': sentiment
-#     }
-#
-
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -92,7 +28,7 @@ class SummaryRequest(BaseModel):
 
 class SummaryResponse(BaseModel):
     summary: str
-    translated_summary: str  # Renamed for clarity
+    translated_summary: str 
     title: str
     author: str
     publication_date: str
